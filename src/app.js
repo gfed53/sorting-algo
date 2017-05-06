38446102f47709b578fae58873ab0d9e901d683d
gfed53 <gfederico7153@gmail.com>
2017-05-06 12:17:19 -0400

    Added sort functionality. Basic implementation in view

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
 (function(){
 	angular
-	.module('myApp', ['ngAnimate', 'ui.router'])
+	.module('myApp', ['ngAnimate'])
 	.run(function(){
 		console.log("Hello World");
 	})
+	.controller('CtrlMain', ['mySorter', CtrlMain]);
 
 })();
+
+
+
+function CtrlMain(mySorter){
+	var vm = this;
+	console.log('Main');
+	vm.items = ['kavxpfequl', 'ynsxcrxout', 'jrhmnjyvpb', 'kefrxznjsn', 'juaypzwoow', 'kaszncrqdc', 'tlvszaghul', 'owfjrozmdf', 'qcwhbmviaj', 'dakguurrhi'];
+	vm.sort = mySorter.sort;
+
+
+
+}
---
+++ src/components/library.js
@@ -0,0 +1,36 @@
+// jshint esversion: 6
+
+(function(){
+	angular
+	.module('myApp')
+	.service('mySorter', [mySorter]);
+
+
+})();
+
+function mySorter(){
+	this.sort = sort;
+
+
+	function swap(items, firstIndex, secondIndex){
+		var temp = items[firstIndex];
+		items[firstIndex] = items[secondIndex];
+		items[secondIndex] = temp;
+	}
+
+	function sort(list){
+		for(let i = 0; i<list.length; i++){
+			console.log('iteration:', i);
+	    	//We chop off i from the length of each iteration because, with each iteration, we will pinpoint the last item that hasn't already been pushed to the end of the array.
+		    for(let j = 0, k = list.length-i; j<k; j++){
+		    	console.log('first:',list[j]);
+		    	console.log('second:',list[j+1]);
+		    	if(list[j] > list[j+1]){
+		    		swap(list, j, j+1);
+		    		console.log('swapped! list is now:', list);
+		    	}
+		    }
+		}
+		return list;
+	}
+}
