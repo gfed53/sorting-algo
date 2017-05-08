7936df26313903ca0a441ddc9b9537a03effa46e
gfed53 <gfederico7153@gmail.com>
2017-05-08 17:52:26 -0400

    Code cleanup, minor readme and comment tweaks

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ I felt that a bubble sort algorithm was the best way to tackle this challenge. T
 
 This is copied from comments in my code explaining the process as I understand it:
 
-"With each iteration, we essentially push the item that should be last to its proper place. Each iteration will traverse one item less than the last since we know the previous last item is where it needs to be. The outer loop using i exists to determine how much less we need to traverse for each iteration. The inner loop is the loop that does the actual checking and swapping if necessary."
+"With each iteration, we essentially push the item that should be last to its proper place. Each iteration will traverse one item less than the last since we know the previous last item is where it needs to be. The outer loop using i exists to determine how much less we need to traverse for each iteration. The inner loop using j is the loop that does the actual checking and swapping if necessary."
 
 This project is also powered by AngularJS, CSS3 transitions, Sass, and Gulp.js build tasks.
+
+Live demo [here](http://www.gregoryfederico.com/sorting-algo/src/)!
 
--- src/components/library.js
+++ src/components/library.js
@@ -22,16 +22,16 @@ function mySorter(){
 		//Create a new instance of array so we can have both unsorted and sorted versions
 		var list = [].slice.call(_list);
 		for(let i = 0; i<list.length; i++){
-	    	//With each iteration, we essentially push the item that should be last to its proper place.
-	    	//Each iteration will traverse one item less than the last since we know the previous last item is where it needs to be.
-	    	//The outer loop using i exists to determine how much less we need to traverse for each iteration.
-	    	//The inner loop is the loop that does the actual checking and swapping if necessary.
-		    for(let j = 0, k = list.length-i; j<k; j++){
-		    	if(list[j] > list[j+1]){
-		    		swap(list, j, j+1);
-		    	}
-		    }
-		}
+    	//With each iteration, we essentially push the item that should be last to its proper place.
+    	//Each iteration will traverse one item less than the last since we know the previous last item is where it needs to be.
+    	//The outer loop using i exists to determine how much less we need to traverse for each iteration.
+    		//The inner loop using j is the loop that does the actual checking and swapping if necessary.
+	    	for(let j = 0, k = list.length-i; j<k; j++){
+	    		if(list[j] > list[j+1]){
+	    			swap(list, j, j+1);
+	    		}
+	    	}
+	    }
 		return list;
 	}
 }
